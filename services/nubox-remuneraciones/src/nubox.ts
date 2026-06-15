eaa8d713fda0ac2edccaa99bcd8e0863e880ec37
Hian Lart <h.lart@gschile.cl>
2026-06-15 19:30:42 -0400

    fix de headless

--- services/nubox-remuneraciones/src/nubox.ts
+++ services/nubox-remuneraciones/src/nubox.ts
@@ -14,7 +14,7 @@ export async function descargarLibroRemuneraciones(
   const { codigoCliente, anio, mes, rut, password } = params;
 
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     ...(process.env.PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD === '1' && {
       executablePath: '/usr/bin/chromium',
